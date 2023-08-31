fb5ed78c0c9e786462d6089f7aa591fabc834ac0
Sopow <Sopowbs@gmail.com>
2023-08-31 11:47:23 +0200

    added meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,41 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content="Sopow" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="google" content="notranslate" />
+        <meta name="theme-color" content={metadata.themeColor} />
+        <meta name="twitter:card" content={metadata.twitterCard} />
+        <meta name="twitter:url" content={metadata.twitterUrl} />
+        <meta name="twitter:title" content={metadata.twitterTitle} />
+        <meta
+          name="twitter:description"
+          content={metadata.twitterDescription}
+        />
+        <meta name="twitter:image" content={metadata.twitterImage} />
+        <meta name="twitter:creator" content="@GSopow" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:site_name" content={metadata.title} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:image:alt" content={metadata.description} />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:locale:alternate" content="fr_FR" />
+
+        <meta charSet={metadata.charSet} />
+        <meta name="viewport" content={metadata.viewport} />
+        <meta
+          name="google-site-verification"
+          content={metadata.googleSiteVerification}
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <div className="text-black h-screen w-screen flex xl:flex-row bg-[#ece7e1] changeable-bg overflow-hidden flex-col">
           <Navbar />
